3ef5464ff791b37446f2e2dc6c937fb76a6aa8a7
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-03-16 15:39:51 +0100

    custom exception

---
+++ src/org/lessons/java/christmas/WishListException.java
@@ -0,0 +1,8 @@
+package org.lessons.java.christmas;
+
+//custom exception
+public class WishListException extends Exception{
+    public WishListException(String message){
+        super(message);
+    }
+}
