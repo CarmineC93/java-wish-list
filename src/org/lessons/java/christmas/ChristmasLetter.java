f2e59d7d49bcd8b53fc98046a0cae4736a8d3c20
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-03-16 15:25:19 +0100

    Second excercise to fix exception in send method

---
+++ src/org/lessons/java/christmas/ChristmasLetter.java
@@ -0,0 +1,78 @@
+package org.lessons.java.christmas;
+
+import java.util.List;
+import java.util.Random;
+
+public class ChristmasLetter {
+
+    //ATTRIBUTES
+        private String sender;
+        private String address;
+        private List<String> wishList;
+
+    //CONSTRUCTORS
+        public ChristmasLetter(String sender, String address, List wishList){
+            this.sender = sender;
+            this.address = address;
+            this.wishList = this.wishList;
+        }
+
+    //GETTER & SETTER
+        public String getSender() {
+            return sender;
+        }
+
+        public void setSender(String sender) {
+            this.sender = sender;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public List<String> getWishList() {
+            return wishList;
+        }
+
+        public void setWishList(List<String> wishList) {
+            this.wishList = wishList;
+        }
+
+        //METHODS
+        @Override
+        public String toString() {
+            return "ChristmasLetter{" +
+                    "sender='" + sender + '\'' +
+                    ", address='" + address + '\'' +
+                    ", wishList=" + wishList +
+                    '}';
+        }
+
+        /*Aggiungere un metodo send(), che prova ad inviare la lettera a Babbo Natale:
+        - se tutto va bene il metodo stampa a video la lettera, il nome e l’indirizzo
+        - se la lista dei desideri supera i 5 elementi, il metodo deve sollevare un’eccezione
+        - calcolare in modo random un booleano che rappresenta se il mittente è stato buono oppure no:
+        se il booleano è false il metodo si chiude sollevando un’eccezione.*/
+
+        public void send() throws WishListException {
+            if (wishList.size() > 5){
+                throw new WishListException("Your list is too long");
+            }
+                Random rnd = new Random();
+                // genera numero casuale tra 0 e 1
+                int GoodOrNot = rnd.nextInt(2);
+
+                String status;
+                if (GoodOrNot == 1){
+                    status = "good";
+                    System.out.println(toString() + "Congrats, you are: " + status + "guy");
+                }else {
+                    status = "bad";
+                    throw new WishListException("Sorry, you are: " + status + "guy");
+                }
+        }
+}
--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -3,17 +3,16 @@ package org.lessons.java.christmas;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws WishListException {
 
-        /*
-
-        creare una List<String> dove salvare la lista dei desideri per Natale.
+        /*        creare una List<String> dove salvare la lista dei desideri per Natale.
         Continuare a chiedere all’utente di inserire un nuovo desiderio nella lista, fino a che l’utente sceglie di fermarsi.
         Ad ogni iterazione mostrare quanti desideri sono già stati espressi e chiedere all’utente se vuole continuare o fermarsi.
         Aggiungere ciascun desiderio alla lista. Al termine dell’inserimento stampare a video la lista ordinata*/
 
-
         List<String> wishList = new ArrayList<String>();
+
+
 
         Scanner scan = new Scanner(System.in);
 
@@ -29,9 +28,17 @@ public class Main {
             stop = scan.nextLine().equalsIgnoreCase("y");
 
         } while(!stop);
-        
+
         Collections.sort(wishList, String.CASE_INSENSITIVE_ORDER);
         System.out.println(wishList);
+
+        ChristmasLetter ginoLetter = new ChristmasLetter("Gino", "Via delle stesse, 5" , wishList);
+
+        try {
+            ginoLetter.send();
+        }catch(WishListException e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
